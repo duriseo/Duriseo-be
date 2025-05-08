4fe774f0121b2e7ea451bc59ae1227e27eeaf861
goldmongmong <jujs45177+3@gmail.com>
2025-05-08 10:58:46 +0900

    initial project setup

---
+++ src/test/java/com/example/duriseo_be/DuriseoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.duriseo_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuriseoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

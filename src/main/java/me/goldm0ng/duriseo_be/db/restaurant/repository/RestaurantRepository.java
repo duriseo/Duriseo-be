4a318c9261247696b020bce68e71188e83349462
goldm0ng <jujs45177@gmail.com>
2025-05-10 05:28:22 +0900

    Refactor: @Repository 추가

--- src/main/java/me/goldm0ng/duriseo_be/db/restaurant/repository/RestaurantRepository.java
+++ src/main/java/me/goldm0ng/duriseo_be/db/restaurant/repository/RestaurantRepository.java
@@ -2,6 +2,12 @@ package me.goldm0ng.duriseo_be.db.restaurant.repository;
 
 import me.goldm0ng.duriseo_be.db.restaurant.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant,Long> {
 }
--- src/main/java/me/goldm0ng/duriseo_be/db/user/repository/UserRepository.java
+++ src/main/java/me/goldm0ng/duriseo_be/db/user/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package me.goldm0ng.duriseo_be.db.user.repository;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import me.goldm0ng.duriseo_be.db.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);

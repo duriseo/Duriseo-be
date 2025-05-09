77d8759918414473c6a2da72ab27cf6c214cdf8d
goldm0ng <jujs45177@gmail.com>
2025-05-10 05:40:05 +0900

    Refactor: 누구나 가게 전체 및 상세 조회를 가능하도록 수정

--- src/main/java/me/goldm0ng/duriseo_be/config/SecurityConfig.java
+++ src/main/java/me/goldm0ng/duriseo_be/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,7 +20,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @RequiredArgsConstructor
 @EnableMethodSecurity(prePostEnabled = true)
-//@EnableWebSecurity
 public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
@@ -35,7 +33,7 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        //.requestMatchers(apiPrefix + "/beneficiaries/**").hasRole("BENEFICIARY")
+                        .requestMatchers(apiPrefix + "/restaurants/**").permitAll()
                         .requestMatchers(apiPrefix + "/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )

f5c05bddb5673f9923278b6040c4dd226bd5a32c
goldm0ng <jujs45177@gmail.com>
2025-05-10 04:16:07 +0900

    Refactor: CORS 설정 추가

--- src/main/java/me/goldm0ng/duriseo_be/config/SecurityConfig.java
+++ src/main/java/me/goldm0ng/duriseo_be/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import me.goldm0ng.duriseo_be.secruity.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -31,6 +32,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         //.requestMatchers(apiPrefix + "/beneficiaries/**").hasRole("BENEFICIARY")
